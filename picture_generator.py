a9a8a51637a55808ff8e974dc13cbef35ce06d01
Ishchild ApartmentAngels <36351054+Wizock@users.noreply.github.com>
2022-04-17 11:56:52 +1000

    Update picture_generator.py

--- picture_generator.py
+++ picture_generator.py
@@ -2,46 +2,42 @@ from PIL import Image, ImageDraw
 
 import random, math, colorsys
 
+
 def generate_art():
-    print('Generate Art')
+    print("Generate Art")
     image_size_px = 2000
-    image_bg_color = (255,255,255,255)
-    image = Image.new(
-        "RGBA",
-        size=(image_size_px, image_size_px),
-        color = image_bg_color
-    )
+    image_bg_color = (255, 255, 255, 255)
+    image = Image.new("RGBA", size=(image_size_px, image_size_px), color=image_bg_color)
     draw = ImageDraw.Draw(image)
     points = []
-    starting_pos = (0,image_size_px*.50),(image_size_px,image_size_px*.50)
+    starting_pos = (0, image_size_px * 0.50), (image_size_px, image_size_px * 0.50)
     x = 200
-    y = image_size_px*.50
+    y = image_size_px * 0.50
     for _ in range(0, math.ceil(1120)):
         x += 70
-        y += (100-random.randint(0, image_size_px*0.3))
-        if y >= image_size_px*0.4: 
-            y -=300
-        elif y <= image_size_px*0.4:
+        y += 100 - random.randint(0, image_size_px * 0.3)
+        if y >= image_size_px * 0.4:
+            y -= 300
+        elif y <= image_size_px * 0.4:
             y += 300
         if x >= image_size_px:
             x -= 300
             break
         newpoints = (x, y)
         points.append(newpoints)
-        y -= (100-random.randint(0, image_size_px*0.4))
+        y -= 100 - random.randint(0, image_size_px * 0.4)
         x += 70
         if x >= image_size_px:
             x -= 300
             break
-        if y >= image_size_px*0.4: 
-            y -=300
-        elif y <= image_size_px*0.4:
+        if y >= image_size_px * 0.4:
+            y -= 300
+        elif y <= image_size_px * 0.4:
             y += 300
         newpoints = (x, y)
         points.append(newpoints)
-        line_xy= (points)
+        line_xy = points
     smoothness_factor = 300
-
 
     for point1, point2 in zip(line_xy[:-1], line_xy[1:]):
         x_diff = point2[0] - point1[0]
@@ -53,18 +49,21 @@ def generate_art():
         x_pos = point1[0]
         y_pos = point1[1]
         for _ in range(smoothness_factor):
-            draw.ellipse((x_pos, y_pos, x_pos+80,y_pos+80), fill=(255,255,255,0))
+            draw.ellipse(
+                (x_pos, y_pos, x_pos + 80, y_pos + 80), fill=(255, 255, 255, 0)
+            )
             x_pos += x_increment
             y_pos += y_increment
-        
-    image.save('front.png')
+
+    image.save("front.png")
 
     return image
 
-def create_grad(name:int):
+
+def create_grad(name: int):
     generate_art()
-    filename = 'front.png'
-    filename1 = 'back_smaller.jpg'
+    filename = "front.png"
+    filename1 = "back_smaller.jpg"
     frontImage = Image.open(filename)
     background = Image.open(filename1)
     frontImage = frontImage.convert("RGBA")
@@ -74,9 +73,11 @@ def create_grad(name:int):
     background.paste(frontImage, (width, height), frontImage)
     background.save(f"pictures/{name}.png", format="png")
 
+
 def make_multiples(amount: int):
     for k in range(0, amount):
         create_grad(k)
 
+
 if __name__ == "__main__":
-    make_multiples(int(input('How Many Copies Would You Like: ')))
+    make_multiples(int(input("How Many Copies Would You Like: ")))
